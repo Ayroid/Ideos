e4d609b8791100e36801c7a87c900d77ba9bab9e
Ayroid <ayushsk0000@gmail.com>
2024-03-11 01:50:58 +0530

    updates

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="homeContainer">
-      <img
-        className="logo"
-        src="/logos/png/logo-no-background.png"
-        alt="logo"
-      />
+      <h1 className="logo">IDEOS</h1>
       <p className="logoSubheading">
         LAUNCHING S<span>OO</span>N<span>!</span>
       </p>
