c4d1b9451b7197d803efac398a93985db2ffd0ce
Ayroid <ayushsk0000@gmail.com>
2024-03-26 00:43:30 +0530

    redux added

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import activeNavigationSlice from "./slices/activeNavigation";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     activeNavigation: activeNavigationSlice,
   },
 });
+
+export default store;
