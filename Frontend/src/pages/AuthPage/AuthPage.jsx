b75b7f87a6d70b58e49e51a27deeda423c5b8a5d
Ayroid <ayushsk0000@gmail.com>
2024-04-17 06:58:44 +0530

    auth animation ready

--- Frontend/src/pages/AuthPage/AuthPage.jsx
+++ Frontend/src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,60 @@
+import { useEffect } from "react";
 import styles from "./AuthPage.module.css";
 
-const { mainDiv, logoText } = styles;
+const {
+  mainDiv,
+  logoText,
+  hiddenLogoText,
+  logoScreen,
+  logoTextAnimation,
+  hiddenLogoTextAnimation,
+  transformedLogoText,
+  transformedHiddenLogoText,
+} = styles;
+
+const initialLoadAnimation = () => {
+  const logoText = document.getElementById("authInitialLogo");
+  const hiddenLogoText = document.getElementById("authInitialHiddenLogo");
+
+  logoText.classList.add(logoTextAnimation);
+  hiddenLogoText.classList.add(hiddenLogoTextAnimation);
+
+  setTimeout(() => {
+    logoText.classList.remove(logoTextAnimation);
+    logoText.classList.add(transformedLogoText);
+    hiddenLogoText.classList.add(transformedHiddenLogoText);
+    hiddenLogoText.classList.remove(hiddenLogoTextAnimation);
+  }, 1000);
+};
+
+// const LoginPage = () => {
+//   return (
+//     <div className="fullWidthContainer">
+//       <div className={mainDiv}>
+//         <h1 className={logoText}>LOGIN</h1>
+//       </div>
+//     </div>
+//   );
+// };
 
 const AuthPage = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      initialLoadAnimation();
+    }, 2000);
+  }, []);
+
   return (
-    <div className="pageContainer">
-      <div className={mainDiv}>
-        <h1 className={logoText}>IDEOS</h1>
+    <div className="fullWidthContainer">
+      <div className={mainDiv} onClick={initialLoadAnimation}>
+        <div className={logoScreen}>
+          <h1 className={logoText} id="authInitialLogo">
+            IDEOS
+          </h1>
+          <h1 className={hiddenLogoText} id="authInitialHiddenLogo">
+            IDEOS
+          </h1>
+        </div>
       </div>
     </div>
   );
